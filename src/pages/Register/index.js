ec0892bf3f97268f58e8ca93e441fb00ed909c87
ihzaeyeshield <ihzaeyeshield21@gmail.com>
2022-07-28 15:11:09 +0700

    final register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -6,20 +6,21 @@ const Register = () => {
   return (
     <div className='main-page'>
       <div className='container'>
-        <div className='container2'>
           <div className='sign'>
             <div className='sign-up'>Sign Up</div>
             <div className='log-in'>Log In</div>
           </div>
           <div className='form'>
             <h2>Sign Up For Free</h2>
-            <input placeholder='First Name' className='text-input'></input>
-            <input placeholder='Last Name' className='text-input'></input>
+            <div className='divide'>
+              <input placeholder='First Name' className='text-input1'></input>
+              <input placeholder='Last Name' className='text-input1'></input>
+            </div>
             <input placeholder='Email' className='text-input'></input>
             <input placeholder='Set a Password' className='text-input'></input>
-            <button>GET STARTED</button>
+            <button className='button-16'><b> GET STARTED</b></button>
           </div>
-        </div>
+        
       </div>
     </div>
   )
